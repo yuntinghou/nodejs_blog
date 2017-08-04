1a5aeea36e88ee20b1e556233cf01b9f369b23bf
yuntinghou <yunting.leo@gmail.com>
2017-08-03 23:35:06 -0700

    Added the config file and figure out some small issues

--- config/default.js
+++ config/default.js
@@ -1,10 +1,11 @@
 module.exports = {
-    port:3000,
-    session: {
+    port: 3000,
+    session:{
         secret: 'cxhounodeblog',
         key: 'cxhounodeblog',
         maxAge: 2592000000
     },
     db: 'cxhoublog',
-    mongodb: 'mongodb://localhost:27107/cxhoublog'
+    mongodb: 'mongodb://localhost/cxhoublog'
+    //mongodb: 'mongodb://cxhou:hou1234@ds034807.mlab.com:34807/mygame'
 };
--- routes/signout.js
+++ routes/signout.js
@@ -1,13 +1,12 @@
+/**
+ * Created by HYT on 8/3/2017.
+ */
 var express = require('express');
 var router = express.Router();
 
 var checkNotLogin = require('../middlewares/check').checkNotLogin;
 
 router.get('/', checkNotLogin, function(req, res, next) {
-    res.send(req.flash());
-});
-
-router.post('/', checkNotLogin, function(req, res, next) {
     res.send(req.flash());
 });
 
